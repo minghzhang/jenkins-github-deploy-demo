5573ba9beec3a990539cd8b3c28d2fd2b44d7ec8
landon.zhang <landon.zhang@zoom.us>
2021-08-24 22:40:29 +0800

    Initial commit

---
+++ src/main/java/com/louis/jenkins/jenkinsgithubdeploydemo/JenkinsGithubDeployDemoApplication.java
@@ -0,0 +1,13 @@
+package com.louis.jenkins.jenkinsgithubdeploydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsGithubDeployDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsGithubDeployDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/louis/jenkins/jenkinsgithubdeploydemo/JenkinsGithubDeployDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.louis.jenkins.jenkinsgithubdeploydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsGithubDeployDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
