699978ce8dc5a69a7e149f0662af30c220d3c487
landon.zhang <landon.zhang@zoom.us>
2021-08-25 21:10:18 +0800

    add WelcomeController

--- src/main/java/com/louis/jenkins/jenkinsgithubdeploydemo/controller/WelcomeController.java
+++ src/main/java/com/louis/jenkins/jenkinsgithubdeploydemo/controller/WelcomeController.java
@@ -3,6 +3,9 @@ package com.louis.jenkins.jenkinsgithubdeploydemo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author : landon
  * @date : 2021/8/25
@@ -11,7 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
 
     @GetMapping("/")
-    public String welcome() {
-        return "Welcome";
+    public String welcome(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/test");
+        return "";
+    }
+
+    @GetMapping("/test")
+    public String testRedirect() {
+
+        return "YES";
     }
 }
